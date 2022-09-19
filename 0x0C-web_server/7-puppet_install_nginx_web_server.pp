c108d494a2bc545202225e78898b488ec75ef43d
ogumaterry <ogumaterry@gmail.com>
2022-09-19 16:05:27 -0700

    using pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# Install and configure nginx
+package { 'jfryman-nginx':
+  ensure => installed,
+}
+
+include nginx
+
+class { 'nginx':
+  manage_repo    => true,
+  package_source => 'nginx-stable',
+}
+
+nginx::resource::server { '34.73.76.135':
+  listen_port      => 80,
+  www_root         => '/var/www/html/',
+  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
+}
+
+file { 'index':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+}
