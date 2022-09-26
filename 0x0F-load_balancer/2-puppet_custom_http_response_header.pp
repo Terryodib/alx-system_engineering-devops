b7e746fe4b4933564153258034c6720989bc10c9
ogumaterry <ogumaterry@gmail.com>
2022-09-26 16:12:34 -0700

    fixing errors

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,69 @@
+# Nginx web server setup and configuration
+
+exec { 'update-apt':
+
+  command => '/usr/bin/apt-get update',
+
+}
+
+
+
+package { 'nginx':
+
+  ensure  => installed,
+
+  require => Exec['update-apt'],
+
+}
+
+
+
+file_line { 'Redirection':
+
+  ensure  => 'present',
+
+  path    => '/etc/nginx/sites-available/default',
+
+  after   => 'listen 80 default_server;',
+
+  line    => 'rewrite ^/redirect_me https://sketchfab.com/bluepeno/models permanent;',
+
+  require => Package['nginx'],
+
+}
+
+
+
+file_line { 'Custom-Header':
+
+  ensure  => 'present',
+
+  path    => '/etc/nginx/sites-available/default',
+
+  after   => 'listen 80 default_server;',
+
+  line    => 'add_header X-Served-By $hostname;',
+
+  require => Package['nginx'],
+
+}
+
+
+
+file { '/var/www/html/index.html':
+
+  content => 'Hello World!',
+
+  require => Package['nginx'],
+
+}
+
+
+
+service { 'nginx':
+
+  ensure  => running,
+
+  require => Package['nginx'],
+
+}
